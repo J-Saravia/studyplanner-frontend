148f625414634b0523e968b029940c63e7eb7a4a
SpielerB <32329914+SpielerB@users.noreply.github.com>
2020-03-27 10:38:18 +0100

    Added BrowserRouter component for react-router-dom Added CssBaseline for material-ui Added ThemeProvider with our MainTheme set as theme Moved page content to the Page component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Page from './page/Page';
+import { BrowserRouter } from 'react-router-dom';
+import { CssBaseline, ThemeProvider } from '@material-ui/core';
+import MainTheme from './MainTheme';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <ThemeProvider theme={MainTheme}>
+                <CssBaseline />
+                <Page />
+            </ThemeProvider>
+        </BrowserRouter>
+    );
 }
 
 export default App;
