27263fa9f60de6b571eb051cbbda73d1c074ddf9
SpielerB <32329914+SpielerB@users.noreply.github.com>
2020-04-01 18:21:54 +0200

    Fixed code layout

--- src/service/WithServices.tsx
+++ src/service/WithServices.tsx
@@ -4,11 +4,10 @@ import { ModuleServiceProps, withModuleService } from './ModuleService';
 import { ModuleVisitServiceProps, withModuleVisitService } from './ModuleVisitService';
 import * as React from 'react';
 
-export interface WithServicesProps extends AuthServiceProps, StudentServiceProps, ModuleServiceProps, ModuleVisitServiceProps {}
+export interface WithServicesProps extends AuthServiceProps, StudentServiceProps, ModuleServiceProps, ModuleVisitServiceProps {
+}
 
-const withServices = <
-    P extends Omit<P, keyof WithServicesProps>
-    >(Component: React.ComponentType<WithServicesProps>): React.FC<P> =>
+const withServices = <P extends Omit<P, keyof WithServicesProps>>(Component: React.ComponentType<WithServicesProps>): React.FC<P> =>
     withAuthService(withStudentService(withModuleService(withModuleVisitService(Component))));
 
 export default withServices;
