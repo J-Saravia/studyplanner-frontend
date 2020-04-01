310ec2a2b977a40faa95e6799e5174cc7df57b39
SpielerB <32329914+SpielerB@users.noreply.github.com>
2020-04-01 18:15:58 +0200

    Refactored DeleteModuleVisitDialog to incorporate the new ConfirmDialog component

--- src/app/semester/dialog/DeleteModuleVisitDialog.tsx
+++ src/app/semester/dialog/DeleteModuleVisitDialog.tsx
@@ -1,35 +1,22 @@
 import * as React from 'react';
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@material-ui/core';
+import ConfirmationDialog from '../../dialog/ConfirmationDialog';
 
 interface DeleteModuleVisitDialogProps {
     open: boolean;
     onCancel: () => void;
-    onDelete: () => void;
+    onConfirm: () => void;
 }
 
 export default class DeleteModuleVisitDialog extends React.Component<DeleteModuleVisitDialogProps, any> {
 
     public render() {
-        const { open, onCancel, onDelete } = this.props;
         return (
-            <Dialog
-                disableBackdropClick
-                disableEscapeKeyDown
-                maxWidth="xs"
-                open={open}
-            >
-                <DialogTitle>Delete Module Visit</DialogTitle>
-                <DialogContent>You are about to delete a module visit, are you sure you want to do this?</DialogContent>
-                <DialogActions>
-                    <Button onClick={onDelete} color="primary">
-                        Delete
-                    </Button>
-                    <Button autoFocus onClick={onCancel} color="primary">
-                        Cancel
-                    </Button>
-                </DialogActions>
-            </Dialog>
+            <ConfirmationDialog
+                title="translation:messages.moduleVisit.delete.title"
+                content="translation:messages.moduleVisit.delete.text"
+                confirmLabel="translation:messages.moduleVisit.delete.confirm"
+                {...this.props}
+            />
         );
     }
-
 }
