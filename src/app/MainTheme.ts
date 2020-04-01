c351aa559251c2000145ba38356eaabed9a3c4e8
SpielerB <32329914+SpielerB@users.noreply.github.com>
2020-04-01 18:17:59 +0200

    Fixed code style

--- src/app/MainTheme.ts
+++ src/app/MainTheme.ts
@@ -3,9 +3,9 @@ import { createMuiTheme, darken, lighten } from '@material-ui/core';
 const MainTheme = createMuiTheme({
     palette: {
         primary: {
-            main: "#1976d2",
-            light: lighten("#1976d2"),
-            dark: darken("#1976d2"),
+            main: '#1976d2',
+            light: lighten('#1976d2'),
+            dark: darken('#1976d2'),
         },
     }
 });
