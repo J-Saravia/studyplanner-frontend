4fd3ec12b8cb4443e00d2ae87652c43c6a549670
SpielerB <32329914+SpielerB@users.noreply.github.com>
2020-04-01 18:17:23 +0200

    Added a reference to already existing user (if present) Added basic routing

--- src/app/page/Page.tsx
+++ src/app/page/Page.tsx
@@ -2,13 +2,15 @@ import * as React from 'react';
 import Menu from './Menu';
 import { Paper, StyledComponentProps, withStyles } from '@material-ui/core';
 import PageStyle from './PageStyle';
-import SemesterList from '../semester/SemesterList';
+import SemesterList from '../semester/list/SemesterList';
 import { AuthServiceProps, withAuthService } from '../../service/AuthService';
 import { ClassNameMap } from '@material-ui/core/styles/withStyles';
 import Login from './login/Login';
 import Student from '../../model/Student';
+import { Switch, Route } from 'react-router-dom';
+import SemesterView from '../semester/detail/SemesterView';
 
-interface PageProps extends AuthServiceProps, StyledComponentProps{
+interface PageProps extends AuthServiceProps, StyledComponentProps {
     classes: ClassNameMap;
 }
 
@@ -21,25 +23,34 @@ class Page extends React.Component<PageProps, PageState> {
 
     constructor(props: Readonly<PageProps>) {
         super(props);
-        this.state = {};
+        this.state = {
+            student: props.authService.getCurrentStudent(),
+        };
     }
 
     componentDidMount() {
-        this.props.authService.addStudentListener(student => this.setState({student}));
+        this.props.authService.addStudentListener(student => this.setState({ student }));
     }
 
     public render() {
         const { classes } = this.props;
         const { student } = this.state;
         if (!student) {
-            return <Login />;
+            return <Login/>;
         }
         return (
             <div className={classes.root}>
-                <Menu />
+                <Menu/>
                 <Paper className={classes.content}>
-                    <div className={classes.toolbar} />
-                    <SemesterList />
+                    <div className={classes.toolbar}/>
+                    <Switch>
+                        <Route path="/semester/:id">
+                            <SemesterView/>
+                        </Route>
+                        <Route>
+                            <SemesterList/>
+                        </Route>
+                    </Switch>
                 </Paper>
             </div>
         );
