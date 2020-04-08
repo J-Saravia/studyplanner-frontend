910693896a94a781ad3844efc03ac4a1e337807c
J-Saravia <58182196+J-Saravia@users.noreply.github.com>
2020-04-08 17:54:20 +0200

    A module visit overview is shown on the semester view. Added Style to the semester view. Started section statistic on semester view.

--- src/app/semester/detail/SemesterView.tsx
+++ src/app/semester/detail/SemesterView.tsx
@@ -1,30 +1,130 @@
 import * as React from 'react';
-import { withRouter, RouteComponentProps } from 'react-router-dom';
-import { ModuleVisitServiceProps, withModuleVisitService } from '../../../service/ModuleVisitService';
-import { AuthServiceProps, withAuthService } from '../../../service/AuthService';
+import {withRouter, RouteComponentProps} from 'react-router-dom';
+import {ModuleVisitServiceProps, withModuleVisitService} from '../../../service/ModuleVisitService';
+import {AuthServiceProps, withAuthService} from '../../../service/AuthService';
 import ModuleVisit from '../../../model/ModuleVisit';
+import {
+    IconButton,
+    StyledComponentProps,
+    Typography,
+    withStyles
+} from "@material-ui/core";
+import SemesterViewStyle from './SemesterViewStyle';
+import {ClassNameMap} from "@material-ui/core/styles/withStyles";
+import SemesterModuleVisit from "../list/SemesterModuleVisit";
+import {AddCircle} from "@material-ui/icons";
+import DeleteModuleVisitDialog from "../dialog/DeleteModuleVisitDialog";
 
-interface SemesterViewProps extends RouteComponentProps<{id: any}>, ModuleVisitServiceProps, AuthServiceProps {
-
+interface SemesterViewProps extends RouteComponentProps<{ id: any }>, StyledComponentProps, ModuleVisitServiceProps, AuthServiceProps {
+    classes: ClassNameMap;
 }
 
-class SemesterView extends React.Component<SemesterViewProps, any> {
+interface SemesterViewState {
+    semesterMap?: ModuleVisit[];
+    semester: string;
+    selectedModuleVisit?: ModuleVisit;
+    deletingModuleVisit?: ModuleVisit;
+}
 
+class SemesterView extends React.Component<SemesterViewProps, SemesterViewState> {
 
     constructor(props: Readonly<SemesterViewProps>) {
         super(props);
-        this.state = {};
+        this.state = {semester: this.props.match.params.id};
     }
 
     componentDidMount(): void {
         let currentStudent = this.props.authService.getCurrentStudent();
-        this.props.moduleVisitService.list(currentStudent?.id as string).then(map => this.setState({visits: map[this.props.match.params.id]}));
+        if (currentStudent) {
+            this.props.moduleVisitService.list(currentStudent.id as string)
+                .then(map => this.setState({semesterMap: map[this.state.semester]}));
+        }
     }
+
+    private moduleVisitClickHandler = (selectedModuleVisit: ModuleVisit) => () => {
+        // TODO: implement this
+    };
+
+    private moduleVisitDeleteHandler = (deletingModuleVisit: ModuleVisit) => () => {
+        this.setState({ deletingModuleVisit })
+    };
+
+    private handleConfirmDelete = () => {
+        const { deletingModuleVisit} = this.state;
+        let { semesterMap } = this.state;
+        if (deletingModuleVisit && semesterMap) {
+            this.props.moduleVisitService.delete(deletingModuleVisit.id as string);
+            semesterMap = semesterMap.filter(mv => (mv.id != deletingModuleVisit.id));
+            this.setState({ semesterMap });
+            this.setState({ deletingModuleVisit: undefined })
+        }
+    };
+
+    private handleCancelDelete = () => {
+        this.setState({ deletingModuleVisit: undefined })
+    };
+
+
+    private handleAddButtonClick = () => {
+        // TODO: implement this
+    };
 
     public render() {
-        return this.state.visits ? this.state.visits.map((m: ModuleVisit) => <div>{m.module.code}</div>) : 'loading';
+        const {classes} = this.props;
+        const {semester, semesterMap} = this.state;
+
+        return (
+            <div className={classes.root}>
+                {this.getOverview(classes, semester, semesterMap)}
+                {SemesterView.getStatistic(classes)}
+
+
+            </div>
+        );
+
     }
 
+    private static getStatistic(classes: ClassNameMap) {
+        return <>
+            <div className={classes.header}>
+                <Typography variant="h6" className={classes.title}>Semesterstatistik</Typography>
+                <hr className={classes.rule}/>
+            </div>
+            Something
+            <p><br/></p>
+        </>;
+    }
+
+    private getOverview(classes: ClassNameMap, semester: string, semesterMap: ModuleVisit[] | undefined) {
+        return <>
+            <div className={classes.header}>
+                <Typography variant="h6" className={classes.title}>{semester}</Typography>
+                <hr className={classes.rule}/>
+            </div>
+            <div className={classes.content}>
+                <div className={classes.modules}>
+                    {semesterMap && semesterMap.map(mv => (
+                        <SemesterModuleVisit
+                            key={mv.id}
+                            moduleVisit={mv}
+                            onClick={this.moduleVisitClickHandler(mv)}
+                            onDelete={this.moduleVisitDeleteHandler(mv)}
+                            isDetailed={true}
+                        />
+                    ))}
+                </div>
+                <div className={classes.buttonSpace}>
+                    <IconButton color="primary" size="medium" onClick={this.handleAddButtonClick}>
+                        <AddCircle
+                            className={classes.button}
+                        />
+                    </IconButton>
+                </div>
+            </div>
+            <DeleteModuleVisitDialog open={!!this.state.deletingModuleVisit} onCancel={this.handleCancelDelete}
+                                     onConfirm={this.handleConfirmDelete}/>
+        </>;
+    }
 }
 
-export default withRouter(withAuthService(withModuleVisitService(SemesterView)));
+export default withRouter(withAuthService(withModuleVisitService(withStyles(SemesterViewStyle)(SemesterView))));
