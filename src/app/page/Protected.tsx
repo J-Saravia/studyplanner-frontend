edcb16b730404f6264ccdc8ed3a9a5500d96ad3c
SpielerB <32329914+SpielerB@users.noreply.github.com>
2020-04-08 22:52:08 +0200

    Added Protected component to conditionally render things based on the auth state

---
+++ src/app/page/Protected.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import * as Rx from 'rxjs';
+import { AuthServiceProps, withAuthService } from '../../service/AuthService';
+import { Trans } from 'react-i18next';
+
+interface ProtectedProps extends AuthServiceProps{
+    fallback?: React.ReactNode;
+    silent?: boolean;
+}
+
+interface ProtectedState {
+    isLoggedIn?: boolean;
+}
+
+/**
+ * This component renders it's children only if the current user is logged in.
+ * It will display a message or fallback ReactNode if not declared as silent.
+ */
+class Protected extends React.Component<ProtectedProps, ProtectedState> {
+
+    private subscription?: Rx.Subscription;
+
+    constructor(props: Readonly<ProtectedProps>) {
+        super(props);
+        this.state = {
+            isLoggedIn: props.authService.isLoggedIn()
+        };
+    }
+
+    public componentDidMount() {
+        this.subscription = this.props.authService
+            .addAuthStateListener(isLoggedIn => this.setState({isLoggedIn}));
+    }
+
+    public componentWillUnmount(){
+        if (this.subscription) {
+            this.subscription.unsubscribe();
+        }
+    }
+
+    public render() {
+        const { silent, fallback, children} = this.props;
+        const { isLoggedIn } = this.state;
+        if (isLoggedIn) {
+            return children;
+        } else if (!silent) {
+            return fallback || (<Trans>translation:messages.unauthorized</Trans>);
+        }
+        return null;
+    }
+
+}
+
+export default withAuthService(Protected);
