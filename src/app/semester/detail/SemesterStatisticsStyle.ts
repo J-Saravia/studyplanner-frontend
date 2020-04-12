c997bccb966d215748291f3d2593e763c0a0d6af
J-Saravia <58182196+J-Saravia@users.noreply.github.com>
2020-04-12 11:53:34 +0200

    Changed semester statistic style to fit mobile use

--- src/app/semester/detail/SemesterStatisticsStyle.ts
+++ src/app/semester/detail/SemesterStatisticsStyle.ts
@@ -1,27 +1,63 @@
-import { createStyles, Theme } from '@material-ui/core';
+import {createStyles, Theme} from '@material-ui/core';
 
 const SemesterStatisticsStyle = (theme: Theme) => createStyles({
     root: {
-        borderRadius: 2,
+        display: 'flex',
+        flexWrap: 'wrap',
+        [theme.breakpoints.down('sm')]: {
+            flexGrow: 1,
+            justifyContent: 'stretch',
+            flexDirection: 'column'
+        }
+
+    },
+
+    group: {
         position: 'relative',
         margin: theme.spacing(1),
-        cursor: 'pointer',
-        '&:active': {
-            boxShadow: 'none'
-        },
+        marginTop: theme.spacing(2),
+        cursor: 'default',
         border: '1px solid black',
-        textAlign: 'left',
+        borderColor: '#FF9633',
+        borderRadius: 2,
+        display: 'flex',
+        [theme.breakpoints.down('sm')]: {
+            flexDirection: 'column',
+            justifyContent: 'stretch',
+            alignItems: 'stretch',
+            justifyItems: 'stretch',
+            flexGrow: 1
+        }
+
+    },
+    groupTitle: {
+        position: 'absolute',
+        transform: `translateX(10%) translateY(-50%) translateY(-${theme.spacing(0.2) + 1}px)`,
+        backgroundColor: 'white',
+        fontWeight: theme.typography.fontWeightBold
+    },
+
+    elem: {
+        borderRadius: 5,
+        margin: theme.spacing(1),
+        cursor: 'default',
+        border: '1px solid black',
+        borderColor: '#FFB066',
         backgroundColor: '#ffeede',
-        color: '#FFB066',
-        borderColor: '#FFB066'
+        minWidth: '100px',
+        [theme.breakpoints.down('sm')]: {
+            flexGrow: 1,
+
+        }
     },
     label: {
         color: theme.palette.common.black,
-        textAlign: 'center'
+        textAlign: 'center',
+        fontWeight: theme.typography.fontWeightBold
     },
-
-    hidden: {
-        display: 'none'
+    value: {
+        color: theme.palette.common.black,
+        textAlign: 'center'
     }
 });
 
