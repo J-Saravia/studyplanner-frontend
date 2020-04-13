e4357ed1e9688611196c208abe96e2718a043a34
SpielerB <32329914+SpielerB@users.noreply.github.com>
2020-04-13 10:35:20 +0200

    Added ModuleInfo to act as combined view for Modules and ModuleVisits

---
+++ src/model/ModuleInfo.ts
@@ -0,0 +1,9 @@
+import Module from './Module';
+
+export default interface ModuleInfo {
+    id: string;
+    module: Module;
+    state: 'failed' | 'blocked' | 'passed' | 'ongoing' | 'planned' | 'none';
+    passedRequirements: boolean;
+    searchString: string;
+}
