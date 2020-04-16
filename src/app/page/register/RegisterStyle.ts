c1b3cec9f319db0f5f9ce2a4c8beb0d18b6432f5
J-Saravia <58182196+J-Saravia@users.noreply.github.com>
2020-04-16 22:36:20 +0200

    Finished Style for the register component.

--- src/app/page/register/RegisterStyle.ts
+++ src/app/page/register/RegisterStyle.ts
@@ -1,9 +1,25 @@
 import { createStyles, Theme } from '@material-ui/core';
 
 const RegisterStyle = (theme: Theme) => createStyles({
-    root: {
-
-    }
+    paper: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding:theme.spacing(4),
+        paddingTop: theme.spacing(2),
+        backgroundColor: theme.palette.common.white,
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.primary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
 });
 
 export default RegisterStyle;
